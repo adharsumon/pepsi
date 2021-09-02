710e0e5f3c8d297bd64abc39e7c8ea9f987bd836
adharsumon <m.sumon7080@gmail.com>
2021-09-02 22:28:15 +0600

    upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    // $('#b1').show();
+    // $('#b2').hide();
+    // $('#b3').hide();
+
+    // $('#bg1').click(function(){
+    //     $('#b1').show();
+    //     $('#b2').hide();
+    //     $('#b3').hide();
+    // });
+    // $('#bg2').click(function(){
+    //     $('#b1').hide();
+    //     $('#b2').show();
+    //     $('#b3').hide();
+    // });
+    // $('#bg3').click(function(){
+    //     $('#b1').hide();
+    //     $('#b2').hide();
+    //     $('#b3').show();
+    // });
+
+    setTimeout(function(){
+        $('#loader').addClass('loaded');
+    }, 3000);
+});
